ed9fe2b62661da6d365dd66048bae259e0aac5de
ShanHng <hngshanhern@gmail.com>
2023-08-26 00:55:31 +0800

    Fix bug where url was using insecure http

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const loader = document.querySelector('.loader')
 const body = document.querySelector('body')
 
 async function getWeatherForecastData (searchKey) {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=aadb07c391574a3fbe4110959231408&q=${searchKey}&days=3&aqi=no&alerts=no`
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=aadb07c391574a3fbe4110959231408&q=${searchKey}&days=3&aqi=no&alerts=no`
   const response = await fetch(url)
   const data = await response.json()
   console.log(data)
